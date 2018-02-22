49486c9aae0326f9ef1e3d40e664b15939991f25
Fahmi <fahmibmz@gmail.com>
2018-02-22 11:26:11 +0100

    firebase added and working commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+const config = {
+  apiKey: "AIzaSyAoLMXOiOFaXinITgSNRPg4eryV1rzxK7g",
+  authDomain: "tp-angularfire-4b679.firebaseapp.com",
+  databaseURL: "https://tp-angularfire-4b679.firebaseio.com",
+  projectId: "tp-angularfire-4b679",
+  storageBucket: "tp-angularfire-4b679.appspot.com",
+  messagingSenderId: "335869683956"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
